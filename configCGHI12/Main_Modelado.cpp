24eb9d76d09b7c76328741d355d91b20797410f5
diego <diego.pibecs@gmail.com>
2026-02-26 18:53:03 -0600

    p3 (con función)

--- configCGHI12/Main_Modelado.cpp
+++ configCGHI12/Main_Modelado.cpp
@@ -15,12 +15,21 @@
 // Shaders
 #include "Shader.h"
 
-const GLint WIDTH = 800, HEIGHT = 600;
+void Inputs(GLFWwindow *window);
+void dibujar(GLint modelLoc, glm::mat4 modelo, GLuint VAO);
+void trasladarEscalar(glm::mat4 modelo, GLint modelLoc, GLuint VAO, float tX, float tY, float tZ, float eX, float eY, float eZ);
 
+const GLint WIDTH = 800, HEIGHT = 600;
+float movX=0.0f;
+float movY=0.0f;
+float movZ=-5.0f;
+float rotX = 0.0f;
+float rotY = 0.0f;
+float rotZ = 0.0f;
 
 int main() {
 	glfwInit();
-	//Verificaciï¿½n de compatibilidad 
+	//Verificación de compatibilidad 
 	// Set all the required options for GLFW
 	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -29,13 +38,13 @@ int main() {
 
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Proyecciones y transformaciones basicas", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Modelado geometrico", nullptr, nullptr);
 
 	int screenWidth, screenHeight;
 
 	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
-	//Verificaciï¿½n de errores de creacion  ventana
+	//Verificación de errores de creacion  ventana
 	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -47,7 +56,7 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glewExperimental = GL_TRUE;
 
-	//Verificaciï¿½n de errores de inicializaciï¿½n de glew
+	//Verificación de errores de inicialización de glew
 
 	if (GLEW_OK != glewInit()) {
 		std::cout << "Failed to initialise GLEW" << std::endl;
@@ -72,51 +81,7 @@ int main() {
 
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
-	// use with Orthographic Projection
 
-	//GLfloat vertices[] = {
- //      -0.5f*500, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
-	//	0.5f * 500, -0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,//Back
-	//	 0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	
-	//	 0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  -0.5f * 500, 0.5f * 500, 0.0f, 0.0f,1.0f,
- //     
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//};
 	
 
 	// use with Perspective Projection
@@ -200,10 +165,12 @@ int main() {
 	
 	glm::mat4 projection=glm::mat4(1);
 
-	projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);//FOV, Radio de aspecto,znear,zfar
+	projection = glm::perspective(glm::radians(45.0f), (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);//FOV, Radio de aspecto,znear,zfar
 	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.0f);//Izq,Der,Fondo,Alto,Cercania,Lejania
 	while (!glfwWindowShouldClose(window))
 	{
+		
+		Inputs(window);
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 
@@ -215,64 +182,65 @@ int main() {
 
 		// Draw our first triangle
 		ourShader.Use();
-		glm::mat4 model = glm::mat4(1);
+		glm::mat4 model=glm::mat4(1);
 		glm::mat4 view=glm::mat4(1);
 	
-		view = glm::translate(view, glm::vec3(0.0f,0.0f,-10.0f));
-		//view = glm::translate( view, glm::vec3( screenWidth / 2, screenHeight / 2,-700.0f ) ); // use with orthographic projection
 
-		model = glm::rotate(model, 0.5f, glm::vec3( 5.0f, 0.0f, 0.0f ) ); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		view = glm::translate(view, glm::vec3(movX,movY, movZ));
+		view = glm::rotate(view, glm::radians(rotX), glm::vec3(1.0f, 0.0f, 0.0f));
+		view = glm::rotate(view, glm::radians(rotY), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::rotate(view, glm::radians(rotZ), glm::vec3(0.0f, 0.0f, 1.0f));
 
-		
 		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-		GLint modelLoc2 = glGetUniformLocation(ourShader.Program, "model");
 		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
 		GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
-		
+
+
 		glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	
+
+
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+	
+		float transformaciones[] = {
+			// traslacion         / escalado
+			 0.0f,  0.75f,  0.0f, 2.5f, 0.05f, 2.5f,
+			 1.0f,   0.0f,  1.0f, 0.1f,  1.5f, 0.1f,
+			 1.0f,   0.0f, -1.0f, 0.1f,  1.5f, 0.1f,
+			-1.0f,   0.0f, -1.0f, 0.1f,  1.5f, 0.1f,
+			-1.0f,   0.0f,  1.0f, 0.1f,  1.5f, 0.1f,
+		};
 
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(3.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, 1.0f, glm::vec3(1.0f, 0.5f, 0.3f));
-		model = glm::rotate(model, 2.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-
-		for (size_t i = 0.0f; i < 100; i++)
+		
+		for (int i = 0; i < sizeof(transformaciones) / sizeof(float); i+=6)
 		{
-			model = glm::rotate(model, 2.0f, glm::vec3(i, 0.0f, 0.0f));
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-			glBindVertexArray(VAO);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
+			trasladarEscalar(model, modelLoc, VAO, transformaciones[i], transformaciones[i+1], transformaciones[i+2], transformaciones[i+3], transformaciones[i+4], transformaciones[i+5]);
 		}
-		model = glm::scale(model, glm::vec3(2.0f, 0.3f, 2.0f));
-
-		
-
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(-3.9f, 0.0f, 0.0f));
-		model = glm::rotate(model, 1.0f, glm::vec3(0.5f, 0.5f, 0.8f));
-		model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
-
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
-
-
-		glBindVertexArray(0);
-
-
-
 		
 		
+		/* Tabla
+		trasladarEscalar(model, modelLoc, VAO, 0.0f, 0.75f, 0.0f, 2.5f, 0.05f, 2.5f);
 		
+		// patas
+		trasladarEscalar(model, modelLoc, VAO, 0.0f, 0.75f, 0.0f, 2.5f, 0.05f, 2.5f);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(1.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 1.5f, 0.1f));
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(1.0f, 0.0f, -1.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 1.5f, 0.1f));
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 1.5f, 0.1f));
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 1.5f, 0.1f));*/
+
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
@@ -284,9 +252,57 @@ int main() {
 
 	glfwTerminate();
 	return EXIT_SUCCESS;
+ }
 
-  
+ void trasladarEscalar(glm::mat4 modelo, GLint modelLoc, GLuint VAO, float tX, float tY, float tZ, float eX, float eY, float eZ) {
+	 modelo = glm::mat4(1.0f);
+	 modelo = glm::translate(modelo, glm::vec3(tX, tY, tZ));
+	 modelo = glm::scale(modelo, glm::vec3(eX, eY, eZ));
+	 glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelo));
+	 glDrawArrays(GL_TRIANGLES, 0, 36);
+	 glBindVertexArray(0);
+	 glBindVertexArray(VAO);
+ }
 
-}
+ void transformarPrisma(glm::mat4 modelo, int transformacion, float x, float y, float z) {
+	 modelo = glm::mat4(1.0f);
+	 if (transformacion == 0) // traslacion
+	 {
+		 modelo = glm::translate(modelo, glm::vec3(x, y, z));
+	 }
+	 else if (transformacion == 1) // rotacion
+	 {
+		 modelo = glm::rotate(modelo, glm::radians(0.5f), glm::vec3(x, y, z));
+	 }
+ }
+
+ void Inputs(GLFWwindow *window) {
+	 if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)  //GLFW_RELEASE
+		 glfwSetWindowShouldClose(window, true);
+	 if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		 movX += 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		 movX -= 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
+		 movY += 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
+		 movY -= 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		 movZ -= 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		 movZ += 0.08f;
+	 if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		 rotY += 0.4f;
+	 if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		 rotY -= 0.4f;
+	 if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		 rotX += 0.4f;
+	 if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		 rotX -= 0.4f;
+	 if (glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS)
+		 rotZ += 0.4f;
+	 if (glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)
+		 rotZ -= 0.4f;
+ }
 
 
